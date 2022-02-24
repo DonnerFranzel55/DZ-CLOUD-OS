4b4995cab1658e05999ed6b862d80d5bd996b5ce
DonnerFranzel55 <98757222+DonnerFranzel55@users.noreply.github.com>
2022-02-24 08:56:22 +0100

    Username change

--- assets/DZ CLOUD OS/JavaScript/Log in/main.js
+++ assets/DZ CLOUD OS/JavaScript/Log in/main.js
@@ -2,7 +2,7 @@ function login() {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-    if (username == 'Devin Zygann' && password == 'StrengGeheim!') {
+    if (username == 'Reiner Zufall' && password == 'StrengGeheim!') {
         window.location.href = './lockscreen.html';
     } else {
         alert('Passwort falsch!');
